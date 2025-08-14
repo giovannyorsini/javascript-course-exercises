672be45b153438f54784632b0a1f04090027d830
Giovanny Orsini da Silva <gikaorsini@gmail.com>
2025-08-14 18:41:07 -0300

    Translate JavaScript.js

--- JS-challenges/c02/JavaScript.js
+++ JS-challenges/c02/JavaScript.js
@@ -1,14 +1,13 @@
 function Click() {
-    let nome = window.prompt("Qual é o seu nome?");
-    
-    if (nome == "") {
-        return alert("[ERRO]Por favor digite um nome")
+    let name = window.prompt("What is your name?");
+    if (name == "") {
+        return alert("[ERROR] Please type a name");
     } else {
-        let idade = window.prompt(`Olá ${nome}! Quantos anos você tem?`);
-            if (idade == "0" || idade == "") {
-            return alert("[ERRO]Por favor digite um número válido!")
+        let age = window.prompt(`Hi ${name}! How old are you?`);
+            if (age == "0" || age == "") {
+            return alert("[ERROR] Please type a valid number!");
     } else {
-        alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`);
-    }
-    } 
+        alert(`I just met ${name}, who is ${age} years old!`);
+    };
+    };
 };
