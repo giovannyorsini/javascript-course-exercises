49e09e386bddf44b1a0fa0e651e95ab0369187e9
Giovanny Orsini da Silva <gikaorsini@gmail.com>
2025-08-17 13:56:12 -0300

    Translate + comments JavaScript.js

--- JS-challenges/c05/JavaScript.js
+++ JS-challenges/c05/JavaScript.js
@@ -1,21 +1,21 @@
 function Click() {
-    let value = window.prompt("Digite uma distância em metros (m)");
-    value = value.replace(",", ".");
-    let val = Number(value);
-    let km = val / 1000;
-    let hm = val / 100;
-    let dam = val / 10;
-    let dm = val * 10;
-    let cm = val * 100;
-    let mm = val * 1000;
-    let res = document.getElementById("res");
+    let value = window.prompt("Enter a distance in meters (m)");    //asks a number from user//
+    value = value.replace(",", ".");        //replace , with .//
+    let val = Number(value);        //turns value string into a number//
+    let km = val / 1000;        //                                         //
+    let hm = val / 100;         //                                         //
+    let dam = val / 10;         //  Gets the val number and                //
+    let dm = val * 10;          //  convert It into every metric measurent //       
+    let cm = val * 100;         //                                         //
+    let mm = val * 1000;        //                                         //
+    let res = document.getElementById("res");        //gets the HTML div//
     res.innerHTML = `
-    <h2>A distância de ${val.toLocaleString()} metros, corresponde a...</h2> 
-    <p>${km.toLocaleString()} quilômetros (Km)</p> 
-    <p>${hm.toLocaleString()} hectômetros (Hm)</p> 
-    <p>${dam.toLocaleString()} decâmetros (Dam)</p> 
-    <p>${dm.toLocaleString()} decímetros (dm)</p> 
-    <p>${cm.toLocaleString()} centímetros (cm)</p> 
-    <p>${mm.toLocaleString()} milímetros (mm)</p>
-    `;
+    <h2>The distance of ${val.toLocaleString()} meters, corresponds to...</h2> 
+    <p>${km.toLocaleString()} kilometers (Km)</p> 
+    <p>${hm.toLocaleString()} hectometers (Hm)</p> 
+    <p>${dam.toLocaleString()} decameters (Dam)</p> 
+    <p>${dm.toLocaleString()} decimeters (dm)</p> 
+    <p>${cm.toLocaleString()} centimeters (cm)</p> 
+    <p>${mm.toLocaleString()} millimeters (mm)</p>
+    `;                                                 //shows every metric measurement in the HTML div and format numbers according to the user’s locale//
 };
