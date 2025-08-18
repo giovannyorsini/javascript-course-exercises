bb47471e8b1f95e4d72a975cacecd94a0e407106
Giovanny Orsini da Silva <gikaorsini@gmail.com>
2025-08-18 20:02:50 -0300

    Translate + comments JavaScript.js

--- JS-challenges/c07/JavaScript.js
+++ JS-challenges/c07/JavaScript.js
@@ -1,16 +1,16 @@
-let cot = window.prompt("Antes de mais nada. Quanto está a cotação do dólar agora?");
-cot = cot.replace(",",".");
-let c = Number(cot);
+let rate = window.prompt("First of all. How much is the dollar rate now?");        //asks a dollar rate to the user//
+rate = rate.replace(",",".");            //replace comma with dot//
+let d = Number(rate);            //turns rate string into a number//
 function Click() {
-    let real = window.prompt("Quantos R$ você tem na carteira?");
-    real = real.replace(",",".");
-    let r = Number(real);
-    let dol = r * c
-    let res = document.getElementById("res");
+    let real = window.prompt("How many R$ do you have in your wallet?");        //starts showing a popup asking for a BRL value//
+    real = real.replace(",",".");            //replace comma with dot//
+    let r = Number(real);                //turns real string into a number//
+    let dol = r * d;                //makes the conversion//
+    let res = document.getElementById("res");            //gets the div where the results will appear//
     res.innerHTML = `
-    <h2>Com R$ ${r.toLocaleString()} (Real) você consegue adquirir: </h2> 
+    <h2>With ${r.toLocaleString(undefined, {style: "currency", currency: "BRL"})} (BRL) you can get: </h2> 
     <p><ul>
-        <li>US$ ${dol.toLocaleString()} (Dólar)
+        <li>${dol.toLocaleString(undefined, {style: "currency", currency: "USD"})} (USD)
     </ul></p>
-    `;
+    `;        //finish the function by showing the conversion of BRL to USD currency//
 };
