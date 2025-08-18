75b825c446ab87a731d0b84814078ad853f534ab
Giovanny Orsini da Silva <gikaorsini@gmail.com>
2025-08-18 08:41:08 -0300

    Translate + comments JavaScript.js

--- JS-challenges/c06/JavaScript.js
+++ JS-challenges/c06/JavaScript.js
@@ -1,13 +1,13 @@
 function Click() {
-    let value = window.prompt("Digite uma temperatura em °C (Celsius)");
-    value = value.replace(",", ".");
-    val = Number(value);
-    let k = val + 273.15
-    let f = val * 1.8 + 32
-    let res = document.getElementById("res");
+    let value = window.prompt("Type a temperature in °C (Celsius)");        //popup asking a number to the user//
+    value = value.replace(",", ".");        //replace commas with a dot//
+    val = Number(value);            //turns value string into a number//
+    let k = val + 273.15        //convert val number to kelvin temperature//
+    let f = val * 1.8 + 32        //convert val number to fahrenheit temperature//
+    let res = document.getElementById("res");        //select the HTML div where the results will appear//
     res.innerHTML = `
-    <h2>A temperatura de ${val.toLocaleString()}°C, corresponde a...</h2> 
+    <h2>The temperature of ${val.toLocaleString()}°C, corresponds to...</h2> 
     <p>${k.toLocaleString()}°K (Kelvin)</p> 
     <p>${f.toLocaleString()}°F (Fahrenheit)</p>
-    `
+    `            //shows the Celsius temperature and all the other conversions//
 };
