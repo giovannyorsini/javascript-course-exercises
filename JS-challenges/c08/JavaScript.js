acb8ac30192c5f51567f76000747ba89dc552184
Giovanny Orsini da Silva <gikaorsini@gmail.com>
2025-08-19 23:19:57 -0300

    Translate + comments JavaScript.js

--- JS-challenges/c08/JavaScript.js
+++ JS-challenges/c08/JavaScript.js
@@ -1,18 +1,18 @@
 function Click() {
-    let name = window.prompt("Qual é o produto que você está comprando?");
-    let value = window.prompt(`Qual é o preço de ${name.toLowerCase()}`);
-    value = value.replace(",",".");
-    let val = Number(value);
-    let res = document.getElementById("res");
-    let dis = val * 0.10;
-    let vald = val - dis;
-    let valStr = val.toLocaleString("pt-br", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-    let disStr = dis.toLocaleString("pt-br", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-    let valdStr = vald.toLocaleString("pt-br", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    let name = window.prompt("What product are you buying?");        //popup asking for a product name//
+    let value = window.prompt(`How much is the ${name.toLowerCase()}`);        //popup asking for a price//
+    value = value.replace(",",".");        //replace the comma with a dot//
+    let val = Number(value);        //turns value string into a number//
+    let res = document.getElementById("res");        //select the HTML div where the results will appear//
+    let dis = val * 0.10;        //calculate the discount//
+    let vald = val - dis;        //calculate the discount on the product price//
+    let valStr = val.toLocaleString(undefined, { style: "currency", currency: "USD" });        //format the val number to currency style//
+    let disStr = dis.toLocaleString(undefined, { style: "currency", currency: "USD" });        //format the dis number to currency style//
+    let valdStr = vald.toLocaleString(undefined, { style: "currency", currency: "USD" });      //format the vald number to currency style//
     res.innerHTML = `
-    <h2>Calculando desconto de 10% para ${name.toLowerCase()}</h2> 
-    <p>O preço original era R$ ${valStr}</p> 
-    <p>Você acaba de ganhar R$ ${disStr} de desconto (-10%)</p> 
-    <p>No fim, você vai pagar R$ ${valdStr} no produto ${name.toLowerCase()}</p>
-    `;
+    <h2>Calculating 10% discount for the ${name.toLowerCase()}</h2> 
+    <p>The original price was ${valStr}</p> 
+    <p>You saved ${disStr} (-10%)</p> 
+    <p>Then, you will pay ${valdStr} in the ${name.toLowerCase()}</p>
+    `;        //shows the original price, then the value of 10% of discount and then the last price after the discount//
 };
