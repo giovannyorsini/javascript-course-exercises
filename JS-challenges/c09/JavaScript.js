20b06fb40cb9b5e317c8e51621cc574fc5cc0a82
Giovanny Orsini da Silva <gikaorsini@gmail.com>
2025-08-20 18:34:10 -0300

    Translate + comments JavaScript.js

--- JS-challenges/c09/JavaScript.js
+++ JS-challenges/c09/JavaScript.js
@@ -1,21 +1,21 @@
 function Click() {
-    let name = window.prompt("Qual é o seu nome?");
-    let sal = (window.prompt(`Qual é o salário de ${name.toUpperCase()}`)).replace(",",".");
-    sal = Number(sal);
-    let perc = (window.prompt(`O salário de ${name.toUpperCase()} vai ser reajustado em qual porcentagem?`)).replace(",",".");
-    perc = Number(perc);
-    let p = perc / 100;
-    let pSal = sal * p;
-    let fSal = sal + pSal;
-    pSal = pSal.toLocaleString("pt-br", {minimumFractionDigits: 2, maximumFractionDigits: 2});
-    fSal = fSal.toLocaleString("pt-br", {minimumFractionDigits: 2, maximumFractionDigits: 2});
-    sal = sal.toLocaleString("pt-br", {minimumFractionDigits: 2, maximumFractionDigits: 2});
-    perc = perc.toLocaleString("pt-br", {maximumFractionDigits: 2});
-    let res = document.getElementById("res");
+    let name = window.prompt("Enter a name:");            //asks for an employee name//
+    let sal = (window.prompt(`What is ${name.toUpperCase()}'s salary?`)).replace(/,/g, "");        //asks for the employee's salary and remove the commas//
+    sal = Number(sal);            //turns sal string into a number//
+    let perc = (window.prompt(`${name.toUpperCase()}'s salary will be adjusted by what percentage?`)).replace(/,/g, "");    //asks for a percentage//
+    perc = Number(perc);        //turns perc string into a number//
+    let p = perc / 100;        //converts perc number to a percentage value//
+    let pSal = sal * p;        //calculates how much is the amount of the salary increase//
+    let fSal = sal + pSal;    //calculates the final salary after the increase//
+    pSal = pSal.toLocaleString(undefined, { style: "currency", currency: "USD" });        //formats to currency style//
+    fSal = fSal.toLocaleString(undefined, { style: "currency", currency: "USD" });        //formats to currency style//
+    sal = sal.toLocaleString(undefined, { style: "currency", currency: "USD" });          //formats to currency style//
+    perc = perc.toLocaleString(undefined, {maximumFractionDigits: 2});        //formats preventing the maximum fraction digits of going over 2//
+    let res = document.getElementById("res");        //gets the HTML div where the results will appear//
     res.innerHTML = `
-        <h2>${name.toUpperCase()} recebeu um aumento salarial!</h2>
-        <p>O salário atual era R$ ${sal}.</p>
-        <p>Com um aumento de ${perc}%, o salário vai aumentar R$ ${pSal} no próximo mês.</p>
-        <p>E a partir daí, ${name.toUpperCase()} vai passar a ganhar R$ ${fSal}.</p>
-    `;
+        <h2>${name.toUpperCase()} received a salary increase!</h2>
+        <p>The current salary was ${sal.toLocaleString(undefined, {style: "currency", currency: "USD"})}.</p>
+        <p>With a ${perc}% increase, the salary will increase by ${pSal} next month.</p>
+        <p>And from then on, ${name.toUpperCase()} will earn ${fSal}.</p>
+    `;        //message showing the initial salary, the percentage, the amount increased and the final salary//
 };
