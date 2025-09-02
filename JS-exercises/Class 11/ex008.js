ccfb8a474ab080a7e23f347a380e0af5afe20a53
Giovanny Orsini da Silva <gikaorsini@gmail.com>
2025-09-02 11:07:51 -0300

    Translate ex008.js
    Translated the script and switched from "var" to "let"

--- JS-exercises/Class 11/ex008.js
+++ JS-exercises/Class 11/ex008.js
@@ -1,6 +1,6 @@
-var vel = 12;
-console.log(`A velocidade do seu carro é ${vel}km/h`);
-if (vel > 60) {  //condição simples!
-    console.log("Você ultrapassou a velocidade permitida. MULTADO!");
+let speed = 12;
+console.log(`The speed of your car is ${speed} mph`);
+if (speed > 60) {
+    console.log("You've exceed the speed limit. You got a ticket!");
 }
-console.log("Dirija sempre usando cinto de segurança!");
+console.log("Drive always wearing seatbelt!");
