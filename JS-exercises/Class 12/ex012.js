89040cb4f3c8ef3424fe607c66402a6e3fef33c7
Giovanny Orsini da Silva <gikaorsini@gmail.com>
2025-09-09 12:28:52 -0300

    Translate ex012.js

--- JS-exercises/Class 12/ex012.js
+++ JS-exercises/Class 12/ex012.js
@@ -1,12 +1,14 @@
-var agora = new Date()
-var hora = agora.getHours()
-console.log(`Agora são exatamente ${hora} horas`);
-if (hora >= 6 && hora <= 12) {
-    console.log("Bom dia");
-} else if (hora <= 18 && hora > 12) {
-    console.log("Boa tarde");
-} else if (hora <= 24 && hora > 18) {
-    console.log ("Boa noite");
+let now = new Date()
+let hour = now.getHours()
+console.log(`Now it's exacty ${hour} hours`);
+if (hour >= 6 && hour <= 12) {
+    console.log("Good Morning");
+} else if (hour <= 18 && hour > 12) {
+    console.log("Good Afternoon");
+} else if (hour <= 24 && hour > 18) {
+    console.log ("Good Evening");
 } else {
-    console.log("Boa madrugada");
+    console.log("Good Morning");
+    // in Brazil we have the word "madrugada" that means any time between 12:01 a.m. and 5 a.m.
+    // "madrugada" doesn't exist in English, so we translate it to "morning"
 }
