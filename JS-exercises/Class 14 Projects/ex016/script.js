68c97d56fe0ee7d750c4ae89b25a10e92ea1ead9
Giovanny Orsini da Silva <gikaorsini@gmail.com>
2025-09-13 08:34:39 -0300

    Translate script.js

--- JS-exercises/Class 14 Projects/ex016/script.js
+++ JS-exercises/Class 14 Projects/ex016/script.js
@@ -1,31 +1,32 @@
-function contar() {
-    let ini = document.getElementById("txti");
-    let fim = document.getElementById("txtf");
-    let passo = document.getElementById("txtp");
+function Count() {
+    let beginning = document.getElementById("txtb");
+    let finish = document.getElementById("txtf");
+    let step = document.getElementById("txts");
     let res = document.getElementById("res");
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = "Impossível contar!";
-        alert("[ERRO] Faltam dados!");
+    if (beginning.value.length == 0 || finish.value.length == 0 || step.value.length == 0) {
+        res.innerHTML = "Impossible to count!";
+        alert("[ERROR] Data missing!");
     } else {
-        res.innerHTML = "contando: <br>";
-        let i = Number(ini.value);
-        let f = Number(fim.value);
-        let p = Number(passo.value);
-        if (p <= 0) {
-            alert("Passo inválido! Considerando PASSO 1");
-            p =1;
+        res.innerHTML = "Counting: <br>";
+        let b = Number(beginning.value);
+        let f = Number(finish.value);
+        let s = Number(step.value);
+        if (s <= 0) {
+            alert("Invalid step! Considering step 1");
+            s = 1;
         };
-        if (i < f) {
-            //contagem crescente
-            for (let c = i; c <= f; c+= p) {
+        if (b < f) {
+            // count up
+            for (let c = b; c <= f; c+= s) {
                 res.innerHTML += `${c} \u{1F449}`;
         };
         } else {
-            //contagem regressiva
-            for (let c = i; c >= f; c-= p){
+            // countdown
+            for (let c = b; c >= f; c-= s){
                 res.innerHTML += `${c} \u{1F449}`;
             };
         };
         res.innerHTML += `\u{1F3C1}`;
     };
+
 };
