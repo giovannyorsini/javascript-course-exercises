514bfc05f13eecd2899f1cf8b387c6bb753fd373
Giovanny Orsini da Silva <gikaorsini@gmail.com>
2025-09-14 10:39:21 -0300

    Update + translate script.js
    Switched from WHILE loop to a FOR loop.

--- JS-exercises/Class 14 Projects/ex017/script.js
+++ JS-exercises/Class 14 Projects/ex017/script.js
@@ -1,18 +1,16 @@
-function tabuada() {
-    let num = document.getElementById("txtn");
-    let tab = document.getElementById("seltab");
-    if (num.value.length == 0) {
-        alert("Por favor, digite um número!");
-    } else {
-        let n = Number(num.value); //converte o valor de num para número 
-        let c = 1;
-        tab.innerHTML = ""; //limpa a tabuada
-        while (c <= 10) { //gera a tabuada
-            let item = document.createElement("option"); //cria os elementos da tabuada
-            item.text = `${n} x ${c} = ${n*c}`;
-            item.value = `tab${c}`
-            tab.appendChild(item);
-            c++;
-        };
-    };
-};
+function Click() {
+  let num = document.getElementById("txtn");
+  let tab = document.getElementById("seltab");
+  if (num.value.length == 0) {
+    alert("Please, enter a number!");
+  } else {
+    let number = Number(num.value);     // converts the value of num to a number 
+    tab.innerHTML = "";     // clean the multiplication table
+    for (let i = 1; i <= 10; i++) {
+      let item = document.createElement("option");     // create the multiplication table content
+      item.text = `${number} x ${i} = ${number*i}`;
+      item.value = `tab${i}`
+      tab.appendChild(item);
+    }
+  }
+}
