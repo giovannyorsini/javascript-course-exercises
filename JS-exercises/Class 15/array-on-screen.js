a770621f638610cfb80dd1d44e8b3e627cf0a901
Giovanny Orsini da Silva <gikaorsini@gmail.com>
2025-09-16 12:26:52 -0300

    Update +  translate array-on-screen.js

--- JS-exercises/Class 15/array-on-screen.js
+++ JS-exercises/Class 15/array-on-screen.js
@@ -1,7 +1,9 @@
-let valores = [8, 1, 7, 4, 2, 9];
-/*for (let pos = 0; pos < valores.length; pos++) {
-    console.log(`A posição ${pos} tem o valor ${valores[pos]}`);
-};*/
-for (let pos in valores) {
-    console.log(`A posição ${pos} tem o valore ${valores[pos]}`);
-};
+let values = [8, 1, 7, 4, 2, 9];
+/*
+for (let position = 0; position < values.length; position++) {
+    console.log(`The position ${position} has the value of ${values[position]}`);
+}
+*/
+for (let position in values) {
+    console.log(`The position ${position} has the value of ${values[position]}`);
+}
